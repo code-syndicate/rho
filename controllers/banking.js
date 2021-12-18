203732d6ae45ef1acf96484a77632c867e9070e0
Timileyin Pelumi <dev.timileyinpelumi@gmail.com>
2021-12-19 00:45:13 +0100

    Changes to be committed: 	modified:   controllers/banking.js 	modified:   views/__transactions__.ejs

--- controllers/banking.js
+++ controllers/banking.js
@@ -1,7 +1,6 @@
+var {body, validationResult} = require('express-validator');
 var Notification = require('./../models/notification');
 var {Deposit, Withdrawal} = require('./../models/transaction');
-
-
 
 function home(req, res) {
 	res.render('index');
@@ -15,69 +14,114 @@ async function deleteNotification(req, res) {
 	res.redirect('/banking/app/');
 }
 
-async function registerDeposit(req, res) {
-	const walletType = req.body.walletType;
-	const amount = req.body.amount;
-	const address = req.body.address;
-	const description = req.body.description;
-	const dateOfTransfer = req.body.date;
+const registerDeposit = [
+	body('walletType', 'Wallet type is required').isAlphanumeric().notEmpty(),
+	body('amount', 'Amount is required').isNumeric().notEmpty(),
+	body('address', 'Wallet address is required')
+		.notEmpty()
+		.isBtcAddress()
+		.withMessage('Please enter a valid wallet address'),
+	body('description').optional(),
+	body('date', 'Date is required')
+		.notEmpty()
 
-	const deposit = new Deposit({
-		amount,
-		description,
-		client: req.user.id,
-		date: dateOfTransfer,
-		walletAdrress: address,
-		walletType,
-		details: `Deposited $${amount} into account`,
-	});
+		.withMessage('Please enter a valid date'),
 
-	await new Notification({
-		listener: req.user.id,
-		description: `Submitted deposit request with reference ID - ${deposit.ref}`,
-	}).save();
+	async function (req, res) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			req.flash('formErrors', errors.array());
+			res.redirect(
+				'/banking/app/?component_ref=transactions&sub_component_ref=D'
+			);
+			return;
+		}
 
-	req.flash(
-		'info',
-		'Your deposit claim has been submitted. Your account will be credited immediately it is verified'
-	);
-	res.locals.flash = true;
+		const walletType = req.body.walletType;
+		const amount = req.body.amount;
+		const address = req.body.address;
+		const description = req.body.description;
+		const dateOfTransfer = req.body.date;
 
-	await deposit.save();
+		const deposit = new Deposit({
+			amount,
+			description,
+			client: req.user.id,
+			date: dateOfTransfer,
+			walletAdrress: address,
+			walletType,
+			details: `Deposited $${amount} into account`,
+		});
 
-	res.redirect('/banking/app/');
-}
+		await new Notification({
+			listener: req.user.id,
+			description: `Submitted deposit request with reference ID - ${deposit.ref}`,
+		}).save();
 
-async function registerWithdrawal(req, res) {
-	const walletType = req.body.walletType;
-	const amount = req.body.amount;
-	const address = req.body.address;
-	const pin = req.body.pin;
+		req.flash(
+			'info',
+			'Your deposit claim has been submitted. Your account will be credited immediately it is verified'
+		);
+		res.locals.flash = true;
 
-	const withdrawal = new Withdrawal({
-		amount,
-		client: req.user.id,
-		walletAdrress: address,
-		walletType,
-		details: `Withdrew $${amount} into ${walletType} wallet address - ${address}`,
-		pin,
-	});
+		await deposit.save();
 
-	await new Notification({
-		listener: req.user.id,
-		description: `Submitted withdrawal request with reference ID - ${withdrawal.ref}`,
-	}).save();
+		res.redirect('/banking/app/');
+	},
+];
 
-	req.flash(
-		'info',
-		'Your withdrawal claim has been submitted. Your wallet will be credited shortly'
-	);
-	res.locals.flash = true;
+const registerWithdrawal = [
+	body('walletType', 'Wallet type is required').isAlphanumeric().notEmpty(),
+	body('amount', 'Amount is required').isNumeric().notEmpty(),
+	body('address', 'Wallet address is required')
+		.notEmpty()
+		.isBtcAddress()
+		.withMessage('Please enter a valid wallet address'),
+	body('pin')
+		.isAlphanumeric()
+		.withMessage('Please enter a valid PIN')
+		.isLength({min: 4, max: 48}),
 
-	await withdrawal.save();
+	async function (req, res) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			req.flash('formErrors', errors.array());
+			res.redirect(
+				'/banking/app/?component_ref=transactions&sub_component_ref=W'
+			);
+			return;
+		}
 
-	res.redirect('/banking/app/');
-}
+		const walletType = req.body.walletType;
+		const amount = req.body.amount;
+		const address = req.body.address;
+		const pin = req.body.pin;
+
+		const withdrawal = new Withdrawal({
+			amount,
+			client: req.user.id,
+			walletAdrress: address,
+			walletType,
+			details: `Withdrew $${amount} into ${walletType} wallet address - ${address}`,
+			pin,
+		});
+
+		await new Notification({
+			listener: req.user.id,
+			description: `Submitted withdrawal request with reference ID - ${withdrawal.ref}`,
+		}).save();
+
+		req.flash(
+			'info',
+			'Your withdrawal claim has been submitted. Your wallet will be credited shortly'
+		);
+		res.locals.flash = true;
+
+		await withdrawal.save();
+
+		res.redirect('/banking/app/');
+	},
+];
 
 async function index(req, res) {
 	let componentRef = req.query.component_ref || 'dashboard';
@@ -128,6 +172,7 @@ async function index(req, res) {
 	res.locals.notifications = notifications;
 	res.locals.user = req.user;
 	res.locals.BTC = 'bc1qhp4ghpz5z6nd60ge7mump80terk022y5gse8f9';
+	res.locals.formErrors = req.flash('formErrors');
 
 	res.render('base', {
 		templateType: componentRef,
