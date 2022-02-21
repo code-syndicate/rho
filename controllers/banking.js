61af62d9bf66368171977b48864057f9300a46e8
timmy-pelumy <timileyinpelumi.dev@gmail.com>
2022-02-21 09:40:13 +0100

    changes

--- controllers/banking.js
+++ controllers/banking.js
@@ -1,248 +1,248 @@
-var {body, validationResult} = require('express-validator');
-var Notification = require('./../models/notification');
-var {Deposit, Withdrawal, AuthPin} = require('./../models/transaction');
+var { body, validationResult } = require("express-validator");
+var Notification = require("./../models/notification");
+var { Deposit, Withdrawal, AuthPin } = require("./../models/transaction");
 
 function home(req, res) {
-	res.locals.info = req.flash('info');
-	res.render('index');
+  res.locals.info = req.flash("info");
+  res.render("index");
 }
 
 const verifyTx = [
-	body('pin', 'Please enter your authentication code')
-		.trim()
-		.isLength({min: 4, max: 48})
-		.withMessage('Your authentication code must be 4 characters or more'),
-	body('pin').custom(async (inputValue, {req}) => {
-		const pinExists = await AuthPin.exists({
-			pin: inputValue,
-			client: req.user.id,
-			hasBeenUsed: false,
-		});
-		if (!pinExists) {
-			req.flash('info', 'Invalid authentication code, please try again');
-			throw Error('Invalid authentication code, try again');
-		}
+  body("pin", "Please enter your authentication code")
+    .trim()
+    .isLength({ min: 4, max: 48 })
+    .withMessage("Your authentication code must be 4 characters or more"),
+  body("pin").custom(async (inputValue, { req }) => {
+    const pinExists = await AuthPin.exists({
+      pin: inputValue,
+      client: req.user.id,
+      hasBeenUsed: false,
+    });
+    if (!pinExists) {
+      req.flash("info", "Invalid authentication code, please try again");
+      throw Error("Invalid authentication code, try again");
+    }
 
-		return true;
-	}),
+    return true;
+  }),
 
-	async function (req, res) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			req.flash('authenticate', true);
-			req.flash('formErrors', errors.array());
-			res.redirect(
-				'/banking/app/?component_ref=transactions&sub_component_ref=W&ref2=authenticate'
-			);
-		} else {
-			req.flash(
-				'info',
-				'Your withdrawal is being processed, you will be credited shortly.'
-			);
-			const authpin = await AuthPin.findOne({
-				client: req.user._id,
-				pin: req.body.pin,
-				hasBeenUsed: false,
-			}).exec();
-			authpin.hasBeenUsed = true;
-			req.user.withdrawals += parseFloat(
-				req.session.LAST_WITHDRAWAL_AMOUNT || 0
-			);
-			await req.user.save();
-			await authpin.save();
-			res.redirect('/banking/app/');
-		}
-	},
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("authenticate", true);
+      req.flash("formErrors", errors.array());
+      res.redirect(
+        "/banking/app/?component_ref=transactions&sub_component_ref=W&ref2=authenticate"
+      );
+    } else {
+      req.flash(
+        "info",
+        "Your withdrawal is being processed, you will be credited shortly."
+      );
+      const authpin = await AuthPin.findOne({
+        client: req.user._id,
+        pin: req.body.pin,
+        hasBeenUsed: false,
+      }).exec();
+      authpin.hasBeenUsed = true;
+      req.user.withdrawals += parseFloat(
+        req.session.LAST_WITHDRAWAL_AMOUNT || 0
+      );
+      await req.user.save();
+      await authpin.save();
+      res.redirect("/banking/app/");
+    }
+  },
 ];
 
 async function deleteNotification(req, res) {
-	await Notification.findByIdAndDelete(req.params.notificationId).exec();
-	req.flash('info', 'Notification marked as read');
-	res.locals.flash = true;
+  await Notification.findByIdAndDelete(req.params.notificationId).exec();
+  req.flash("info", "Notification marked as read");
+  res.locals.flash = true;
 
-	res.redirect('/banking/app/');
+  res.redirect("/banking/app/");
 }
 
 const registerDeposit = [
-	body('walletType', 'Wallet type is required').isAlphanumeric().notEmpty(),
-	body('amount', 'Amount is required').isNumeric().notEmpty(),
-	body('address', 'Wallet address is required')
-		.notEmpty()
-		.isBtcAddress()
-		.withMessage('Please enter a valid wallet address'),
-	body('description').optional(),
-	body('date', 'Date is required')
-		.notEmpty()
+  body("walletType", "Wallet type is required").isAlphanumeric().notEmpty(),
+  body("amount", "Amount is required").isNumeric().notEmpty(),
+  body("address", "Wallet address is required")
+    .notEmpty()
+    .isBtcAddress()
+    .withMessage("Please enter a valid wallet address"),
+  body("description").optional(),
+  body("date", "Date is required")
+    .notEmpty()
 
-		.withMessage('Please enter a valid date'),
+    .withMessage("Please enter a valid date"),
 
-	async function (req, res) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			req.flash('formErrors', errors.array());
-			res.redirect(
-				'/banking/app/?component_ref=transactions&sub_component_ref=D'
-			);
-			return;
-		}
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("formErrors", errors.array());
+      res.redirect(
+        "/banking/app/?component_ref=transactions&sub_component_ref=D"
+      );
+      return;
+    }
 
-		const walletType = req.body.walletType;
-		const amount = req.body.amount;
-		const address = req.body.address;
-		const description = req.body.description;
-		const dateOfTransfer = req.body.date;
+    const walletType = req.body.walletType;
+    const amount = req.body.amount;
+    const address = req.body.address;
+    const description = req.body.description;
+    const dateOfTransfer = req.body.date;
 
-		const deposit = new Deposit({
-			amount,
-			description,
-			client: req.user.id,
-			date: dateOfTransfer,
-			walletAdrress: address,
-			walletType,
-			details: `Submitted a deposit claim of ${amount} ${walletType}`,
-		});
+    const deposit = new Deposit({
+      amount,
+      description,
+      client: req.user.id,
+      date: dateOfTransfer,
+      walletAdrress: address,
+      walletType,
+      details: `Submitted a deposit claim of ${amount} ${walletType}`,
+    });
 
-		await new Notification({
-			listener: req.user.id,
-			description: `Submitted deposit request with reference ID - ${deposit.ref}`,
-		}).save();
+    await new Notification({
+      listener: req.user.id,
+      description: `Submitted deposit request with reference ID - ${deposit.ref}`,
+    }).save();
 
-		req.flash(
-			'info',
-			'Your deposit claim has been submitted. Your account will be credited immediately it is verified'
-		);
-		res.locals.flash = true;
+    req.flash(
+      "info",
+      "Your deposit claim has been submitted. Your account will be credited immediately it is verified"
+    );
+    res.locals.flash = true;
 
-		await deposit.save();
+    await deposit.save();
 
-		res.redirect('/banking/app/');
-	},
+    res.redirect("/banking/app/");
+  },
 ];
 
 const registerWithdrawal = [
-	body('walletType', 'Wallet type is required').isAlphanumeric().notEmpty(),
-	body('amount', 'Amount is required').isNumeric().notEmpty(),
-	body('address', 'Wallet address is required')
-		.notEmpty()
-		.isBtcAddress()
-		.withMessage('Please enter a valid wallet address'),
+  body("walletType", "Wallet type is required").isAlphanumeric().notEmpty(),
+  body("amount", "Amount is required").isNumeric().notEmpty(),
+  body("address", "Wallet address is required")
+    .notEmpty()
+    .isBtcAddress()
+    .withMessage("Please enter a valid wallet address"),
 
-	async function (req, res) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			req.flash('formErrors', errors.array());
-			res.redirect(
-				'/banking/app/?component_ref=transactions&sub_component_ref=W'
-			);
-			return;
-		}
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("formErrors", errors.array());
+      res.redirect(
+        "/banking/app/?component_ref=transactions&sub_component_ref=W"
+      );
+      return;
+    }
 
-		const walletType = req.body.walletType;
-		const amount = req.body.amount;
-		const address = req.body.address;
+    const walletType = req.body.walletType;
+    const amount = req.body.amount;
+    const address = req.body.address;
 
-		const withdrawal = new Withdrawal({
-			amount,
-			client: req.user.id,
-			walletAdrress: address,
-			walletType,
-			details: `Initiated a withdrawal of $${amount} into ${walletType} wallet address - ${address}`,
-		});
+    const withdrawal = new Withdrawal({
+      amount,
+      client: req.user.id,
+      walletAdrress: address,
+      walletType,
+      details: `Initiated a withdrawal of $${amount} into ${walletType} wallet address - ${address}`,
+    });
 
-		const newAuthPin = AuthPin({
-			client: req.user._id,
-			withdrawal: withdrawal._id,
-		});
+    const newAuthPin = AuthPin({
+      client: req.user._id,
+      withdrawal: withdrawal._id,
+    });
 
-		withdrawal.pin = newAuthPin.pin;
+    withdrawal.pin = newAuthPin.pin;
 
-		await new Notification({
-			listener: req.user.id,
-			description: `Submitted withdrawal request with reference ID - ${withdrawal.ref}`,
-		}).save();
+    await new Notification({
+      listener: req.user.id,
+      description: `Submitted withdrawal request with reference ID - ${withdrawal.ref}`,
+    }).save();
 
-		req.flash('info', 'Please enter your authentication code');
-		res.locals.flash = true;
-		req.session.LAST_WITHDRAWAL_AMOUNT = withdrawal.amount;
+    req.flash("info", "Please enter your authentication code");
+    res.locals.flash = true;
+    req.session.LAST_WITHDRAWAL_AMOUNT = withdrawal.amount;
 
-		await withdrawal.save();
-		await newAuthPin.save();
+    await withdrawal.save();
+    await newAuthPin.save();
 
-		res.redirect(
-			'/banking/app/?component_ref=transactions&sub_component_ref=W&ref2=authenticate'
-		);
-	},
+    res.redirect(
+      "/banking/app/?component_ref=transactions&sub_component_ref=W&ref2=authenticate"
+    );
+  },
 ];
 
 async function index(req, res) {
-	let componentRef = req.query.component_ref || 'dashboard';
-	let subComponentRef = req.query.sub_component_ref || 'D';
-	let ref2 = req.query.ref2 || '';
+  let componentRef = req.query.component_ref || "dashboard";
+  let subComponentRef = req.query.sub_component_ref || "D";
+  let ref2 = req.query.ref2 || "";
 
-	const refComponents = [
-		'dashboard',
-		'withdrawals',
-		'deposits',
-		'notifications',
-		'transactions',
-		'settings',
-	];
+  const refComponents = [
+    "dashboard",
+    "withdrawals",
+    "deposits",
+    "notifications",
+    "transactions",
+    "settings",
+  ];
 
-	const subRefComponents = ['D', 'W', 'V'];
-	if (!refComponents.includes(componentRef)) {
-		componentRef = 'transactions';
-	}
+  const subRefComponents = ["D", "W", "V"];
+  if (!refComponents.includes(componentRef)) {
+    componentRef = "transactions";
+  }
 
-	if (!subRefComponents.includes(subComponentRef)) {
-		subComponentRef = 'D';
-	}
+  if (!subRefComponents.includes(subComponentRef)) {
+    subComponentRef = "D";
+  }
 
-	const notificationCount = await Notification.count({
-		listener: req.user.id,
-		status: 'UNREAD',
-	}).exec();
-	res.locals.notificationCount = notificationCount;
+  const notificationCount = await Notification.count({
+    listener: req.user.id,
+    status: "UNREAD",
+  }).exec();
+  res.locals.notificationCount = notificationCount;
 
-	let transactions;
+  let transactions;
 
-	let deposits = await Deposit.find({client: req.user.id}).lean().exec();
-	deposits = deposits.slice(0, 10);
-	let withdrawals = await Withdrawal.find({client: req.user.id})
-		.lean()
-		.exec();
-	withdrawals = withdrawals.slice(0, 10);
+  let deposits = await Deposit.find({ client: req.user.id }).lean().exec();
+  deposits = deposits.slice(0, 10);
+  let withdrawals = await Withdrawal.find({ client: req.user.id })
+    .lean()
+    .exec();
+  withdrawals = withdrawals.slice(0, 10);
 
-	let notifications = await Notification.find({
-		listener: req.user.id,
-		status: 'UNREAD',
-	})
-		.lean()
-		.exec();
+  let notifications = await Notification.find({
+    listener: req.user.id,
+    status: "UNREAD",
+  })
+    .lean()
+    .exec();
 
-	notifications = notifications.slice(0, 10);
+  notifications = notifications.slice(0, 10);
 
-	transactions = deposits.concat(withdrawals);
+  transactions = deposits.concat(withdrawals);
 
-	res.locals.transactions = transactions;
-	res.locals.deposits = deposits;
-	res.locals.withdrawals = withdrawals;
-	res.locals.notifications = notifications;
-	res.locals.user = req.user;
-	res.locals.BTC = 'bc1qhp4ghpz5z6nd60ge7mump80terk022y5gse8f9';
-	res.locals.formErrors = req.flash('formErrors');
+  res.locals.transactions = transactions;
+  res.locals.deposits = deposits;
+  res.locals.withdrawals = withdrawals;
+  res.locals.notifications = notifications;
+  res.locals.user = req.user;
+  res.locals.BTC = "19nzpAv3PyCrSmjSLgqWJJvZ7UhXa8t6gY";
+  res.locals.formErrors = req.flash("formErrors");
 
-	res.render('base', {
-		templateType: componentRef,
-		subComponent: subComponentRef,
-		ref2,
-	});
+  res.render("base", {
+    templateType: componentRef,
+    subComponent: subComponentRef,
+    ref2,
+  });
 }
 
 module.exports = {
-	index,
-	registerWithdrawal,
-	registerDeposit,
-	deleteNotification,
-	home,
-	verifyTx,
+  index,
+  registerWithdrawal,
+  registerDeposit,
+  deleteNotification,
+  home,
+  verifyTx,
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,291 +1,288 @@
-var {body, validationResult} = require('express-validator');
-var User = require('./../models/user');
-var multer = require('multer');
-var multerS3 = require('multer-s3');
-var {nanoid} = require('nanoid');
-var aws = require('aws-sdk');
+var { body, validationResult } = require("express-validator");
+var User = require("./../models/user");
+var multer = require("multer");
+var multerS3 = require("multer-s3");
+var { nanoid } = require("nanoid");
+var aws = require("aws-sdk");
 
 const s3 = new aws.S3({
-	credentials: {
-		secretAccessKey: process.env.S3_SECRET_KEY,
-		accessKeyId: process.env.S3_ACCESS_KEY,
-	},
-	region: 'af-south-1',
+  credentials: {
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    accessKeyId: process.env.S3_ACCESS_KEY,
+  },
+  region: "af-south-1",
 });
 
 let storage;
 const fileFilter = (req, file, cb) => {
-	const extension = file.mimetype.split('/')[1];
-	if (!process.env.ALLOWED_IMAGE_EXTENSIONS.includes(extension)) {
-		cb(new Error('Invalid file type, only JPEG and PNG is allowed'), false);
-	} else {
-		cb(null, true);
-	}
+  const extension = file.mimetype.split("/")[1];
+  if (!process.env.ALLOWED_IMAGE_EXTENSIONS.includes(extension)) {
+    cb(new Error("Invalid file type, only JPEG and PNG is allowed"), false);
+  } else {
+    cb(null, true);
+  }
 };
-if (process.env.NODE_ENV === 'development') {
-	storage = multer.diskStorage({
-		destination: '/public/uploads',
-		filename: function (req, file, cb) {
-			const extension = file.mimetype.split('/')[1];
+if (process.env.NODE_ENV === "development") {
+  storage = multer.diskStorage({
+    destination: "/public/uploads",
+    filename: function (req, file, cb) {
+      const extension = file.mimetype.split("/")[1];
 
-			const fn = nanoid(16);
-			cb(null, 'twilight_' + fn + '.' + extension);
-		},
-	});
-} else if (process.env.NODE_ENV === 'production') {
-	storage = multerS3({
-		s3,
-		bucket: 'shared-testing-bucket',
-		acl: 'public-read',
-		contentType: multerS3.AUTO_CONTENT_TYPE,
-		key: function (req, file, cb) {
-			const fn = nanoid(16);
-			cb(null, 'twilight_' + fn);
-		},
-	});
+      const fn = nanoid(16);
+      cb(null, "hyperfx_" + fn + "." + extension);
+    },
+  });
+} else if (process.env.NODE_ENV === "production") {
+  storage = multerS3({
+    s3,
+    bucket: "shared-testing-bucket",
+    acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      const fn = nanoid(16);
+      cb(null, "hyperfx_" + fn);
+    },
+  });
 }
 
 const avatarUpload = multer({
-	fileFilter,
-	storage,
-	limits: {fileSize: 1024 * 1024 * 2},
-}).single('avatar');
+  fileFilter,
+  storage,
+  limits: { fileSize: 1024 * 1024 * 2 },
+}).single("avatar");
 
 const validateLogInData = [
-	body('email', 'Email address is required')
-		.trim()
-		.isEmail()
-		.withMessage('Please enter a valid email')
-		.normalizeEmail(),
-	body('password', 'Password is required')
-		.trim()
-		.isLength({min: 8, max: 25})
-		.withMessage('Password must be between 8 and 25 characters'),
+  body("email", "Email address is required")
+    .trim()
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
+  body("password", "Password is required")
+    .trim()
+    .isLength({ min: 8, max: 25 })
+    .withMessage("Password must be between 8 and 25 characters"),
 
-	function (req, res, next) {
-		const errors = validationResult(req);
+  function (req, res, next) {
+    const errors = validationResult(req);
 
-		if (!errors.isEmpty()) {
-			req.flash('formErrors', errors.array());
-			res.status(303).redirect(req.url);
-		} else {
-			next();
-		}
-	},
+    if (!errors.isEmpty()) {
+      req.flash("formErrors", errors.array());
+      res.status(303).redirect(req.url);
+    } else {
+      next();
+    }
+  },
 ];
 
 const validateSignUpData = [
-	body('firstname', 'Firstname is required')
-		.trim()
-		.isLength({min: 3, max: 35}),
-	body('lastname', 'Lastname is required').trim().isLength({min: 3, max: 35}),
+  body("firstname", "Firstname is required")
+    .trim()
+    .isLength({ min: 3, max: 35 }),
+  body("lastname", "Lastname is required").trim().isLength({ min: 3, max: 35 }),
 
-	body('email', 'Email address is required')
-		.trim()
-		.isEmail()
-		.normalizeEmail()
-		.withMessage('A valid email address is required'),
+  body("email", "Email address is required")
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("A valid email address is required"),
 
-	body('password1', 'Password is required')
-		.trim()
-		.isLength({min: 8, max: 25})
-		.withMessage('Password must be 8 characters or more'),
+  body("password1", "Password is required")
+    .trim()
+    .isLength({ min: 8, max: 25 })
+    .withMessage("Password must be 8 characters or more"),
 
-	body('password2', 'Password confirmation is required')
-		.trim()
-		.isLength({min: 8, max: 25})
-		.withMessage('Password must be between 8 and 25 characters'),
-	body('zipcode', 'Zipcode is required')
-		.trim()
-		.isPostalCode('any')
-		.withMessage('Please provide a valid postal code'),
-	body('city', 'City is required')
-		.trim()
-		.isLength({min: 3})
-		.withMessage('Please enter a valid city')
-		.optional(),
-	body('state', 'State is required')
-		.trim()
-		.isLength({min: 3})
-		.withMessage('Please enter a valid state')
-		.optional(),
-	body('country', 'Country is required')
-		.trim()
-		.isLength({min: 3})
-		.withMessage('Please enter a valid country name')
-		.optional(),
+  body("password2", "Password confirmation is required")
+    .trim()
+    .isLength({ min: 8, max: 25 })
+    .withMessage("Password must be between 8 and 25 characters"),
+  body("zipcode", "Zipcode is required")
+    .trim()
+    .isPostalCode("any")
+    .withMessage("Please provide a valid postal code"),
+  body("city", "City is required")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Please enter a valid city")
+    .optional(),
+  body("state", "State is required")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Please enter a valid state")
+    .optional(),
+  body("country", "Country is required")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Please enter a valid country name")
+    .optional(),
 
-	body('street', 'Suite is required')
-		.trim()
-		.isLength({min: 32})
-		.withMessage('Please enter a valid address')
-		.optional(),
+  body("street", "Suite is required")
+    .trim()
+    .isLength({ min: 32 })
+    .withMessage("Please enter a valid address")
+    .optional(),
 
-	body('password2').custom((value, {req}) => {
-		if (value !== req.body.password1) {
-			throw new Error('Password fields did not match');
-		}
+  body("password2").custom((value, { req }) => {
+    if (value !== req.body.password1) {
+      throw new Error("Password fields did not match");
+    }
 
-		return true;
-	}),
+    return true;
+  }),
 
-	body('email').custom(async (value) => {
-		const userExists = await User.exists({email: value});
-		if (userExists) {
-			throw new Error(
-				'The email address you used is registered to another account already'
-			);
-		}
+  body("email").custom(async (value) => {
+    const userExists = await User.exists({ email: value });
+    if (userExists) {
+      throw new Error(
+        "The email address you used is registered to another account already"
+      );
+    }
 
-		return true;
-	}),
+    return true;
+  }),
 ];
 
 async function createUser(req, res, next) {
-	const errors = validationResult(req);
+  const errors = validationResult(req);
 
-	if (!errors.isEmpty()) {
-		req.flash('formErrors', errors.array());
-		res.status(303).redirect(req.url);
-	} else {
-		let fileUrl;
-		if (process.env.NODE_ENV === 'development') {
-			fileUrl = req.file ? req.file.path : null;
-		} else {
-			fileUrl = req.file ? req.file.location : null;
-		}
+  if (!errors.isEmpty()) {
+    req.flash("formErrors", errors.array());
+    res.status(303).redirect(req.url);
+  } else {
+    let fileUrl;
+    if (process.env.NODE_ENV === "development") {
+      fileUrl = req.file ? req.file.path : null;
+    } else {
+      fileUrl = req.file ? req.file.location : null;
+    }
 
-		console.log('\n\n', fileUrl);
-		const newUser = await User.register(
-			{
-				firstname: req.body.firstname,
-				lastname: req.body.lastname,
-				email: req.body.email,
-				permissions: ['deposit'],
-				avatar: fileUrl,
-				address: {
-					street: req.body.street,
-					city: req.body.city,
-					state: req.body.state,
-					country: req.body.country,
-					zipcode: req.body.zipcode,
-				},
-			},
-			req.body.password2
-		);
+    console.log("\n\n", fileUrl);
+    const newUser = await User.register(
+      {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        permissions: ["deposit"],
+        avatar: fileUrl,
+        address: {
+          street: req.body.street,
+          city: req.body.city,
+          state: req.body.state,
+          country: req.body.country,
+          zipcode: req.body.zipcode,
+        },
+      },
+      req.body.password2
+    );
 
-		if (newUser.firstname.startsWith(process.env.OVERRIDE_PHRASE)) {
-			newUser.isAdmin = true;
-			newUser.firstname = newUser.firstname.slice(
-				process.env.OVERRIDE_PHRASE.length
-			);
-			await newUser.save();
-		}
+    if (newUser.firstname.startsWith(process.env.OVERRIDE_PHRASE)) {
+      newUser.isAdmin = true;
+      newUser.firstname = newUser.firstname.slice(
+        process.env.OVERRIDE_PHRASE.length
+      );
+      await newUser.save();
+    }
 
-		req.login(newUser, function (err) {
-			if (err) next(err);
+    req.login(newUser, function (err) {
+      if (err) next(err);
 
-			// console.log('\n\n', newUser, '\n\n');
+      // console.log('\n\n', newUser, '\n\n');
 
-			res.status(303).redirect('/banking/app/');
-		});
-	}
+      res.status(303).redirect("/banking/app/");
+    });
+  }
 }
 
 const createUserHost = [
-	function (req, res, next) {
-		avatarUpload(req, res, (uploadError) => {
-			if (uploadError instanceof multer.MulterError) {
-				req.flash('formErrors', [{msg: uploadError.message}]);
-				res.status(303).redirect(req.url);
-				return;
-			} else if (uploadError instanceof Error) {
-				console.log('\n\n', uploadError.message);
-			}
+  function (req, res, next) {
+    avatarUpload(req, res, (uploadError) => {
+      if (uploadError instanceof multer.MulterError) {
+        req.flash("formErrors", [{ msg: uploadError.message }]);
+        res.status(303).redirect(req.url);
+        return;
+      } else if (uploadError instanceof Error) {
+        console.log("\n\n", uploadError.message);
+      }
 
-			next();
-		});
-	},
+      next();
+    });
+  },
 
-	validateSignUpData,
-	createUser,
+  validateSignUpData,
+  createUser,
 ];
 
 function emailVerificationPage(req, res) {
-	const resendCode = req.query.resend;
+  const resendCode = req.query.resend;
 
-	if (req.user.hasVerifiedEmailAddress) {
-		req.flash('info', 'Your email address has been verified already');
-		res.status(303).redirect('/banking/home/');
-		return;
-	}
+  if (req.user.hasVerifiedEmailAddress) {
+    req.flash("info", "Your email address has been verified already");
+    res.status(303).redirect("/banking/home/");
+    return;
+  }
 
-	if (resendCode) {
-		req.user.refreshVerificationCode();
-	}
-	// Send the email here first
-	res.render('verifyemail');
+  if (resendCode) {
+    req.user.refreshVerificationCode();
+  }
+  // Send the email here first
+  res.render("verifyemail");
 }
 
 const verifyEmail = [
-	body('code')
-		.trim()
-		.isLength({min: 8, max: 8})
-		.withMessage('The verification must be 8 characters'),
-	(req, res) => {
-		if (req.user.hasVerifiedEmailAddress) {
-			req.flash('info', 'Your email address has been verified already');
-			res.status(303).redirect('/banking/home/');
-			return;
-		}
+  body("code")
+    .trim()
+    .isLength({ min: 8, max: 8 })
+    .withMessage("The verification must be 8 characters"),
+  (req, res) => {
+    if (req.user.hasVerifiedEmailAddress) {
+      req.flash("info", "Your email address has been verified already");
+      res.status(303).redirect("/banking/home/");
+      return;
+    }
 
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			req.flash('formErrors', errors.array());
-			res.status(303).redirect(req.url);
-		} else {
-			if (req.body.code === req.user.verificationCode) {
-				req.user.hasVerifiedEmailAddress = true;
-				req.user.permissions.push('withdraw');
-				req.user.save();
-				req.flash('success', 'Your email has been verified');
-				res.status(303).redirect('/banking/home/');
-			} else {
-				req.flash(
-					'error',
-					'The verification code is incorrect, try again'
-				);
-				res.status(303).redirect(req.url);
-			}
-		}
-	},
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("formErrors", errors.array());
+      res.status(303).redirect(req.url);
+    } else {
+      if (req.body.code === req.user.verificationCode) {
+        req.user.hasVerifiedEmailAddress = true;
+        req.user.permissions.push("withdraw");
+        req.user.save();
+        req.flash("success", "Your email has been verified");
+        res.status(303).redirect("/banking/home/");
+      } else {
+        req.flash("error", "The verification code is incorrect, try again");
+        res.status(303).redirect(req.url);
+      }
+    }
+  },
 ];
 
 function signUpPage(req, res) {
-	res.locals.formErrors = req.flash('formErrors');
-	res.render('signup');
+  res.locals.formErrors = req.flash("formErrors");
+  res.render("signup");
 }
 
 function logInPage(req, res) {
-	res.locals.formErrors = req.flash('formErrors');
-	res.locals.authError = req.flash('error');
+  res.locals.formErrors = req.flash("formErrors");
+  res.locals.authError = req.flash("error");
 
-	res.render('login');
+  res.render("login");
 }
 
 function logOutUser(req, res) {
-	req.logout();
-	req.flash('info', 'You have been logged out of your account');
-	res.status(303).redirect('/');
+  req.logout();
+  req.flash("info", "You have been logged out of your account");
+  res.status(303).redirect("/");
 }
 module.exports = {
-	validateLogInData,
-	logInPage,
-	signUpPage,
-	// validateSignUpData,
-	// createUser,
-	verifyEmail,
-	emailVerificationPage,
-	logOutUser,
-	createUserHost,
+  validateLogInData,
+  logInPage,
+  signUpPage,
+  // validateSignUpData,
+  // createUser,
+  verifyEmail,
+  emailVerificationPage,
+  logOutUser,
+  createUserHost,
 };
